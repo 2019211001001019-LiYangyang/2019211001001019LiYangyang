3e88304542dda9245253db4292273857743c3d98
2019211001001019LiYangyang <1954585880@qq.com>
2021-04-06 18:51:46 +0800

    HW-Week 4 Commit

---
+++ src/main/java/com/LiYangyang/week4/demo/ConfigDemoServlet.java
@@ -0,0 +1,31 @@
+package com.LiYangyang.week4.demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name="name",value="Li Yangyang"),
+                @WebInitParam(name="studentId",value="2019211001001019"),
+        }
+)
+public class ConfigDemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletConfig config=getServletConfig();
+        String name=config.getInitParameter("name");
+        String studentId=config.getInitParameter("studentId");
+        PrintWriter writer= response.getWriter();
+        writer.println("Name : "+name);
+        writer.println("StudentID : "+studentId);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
