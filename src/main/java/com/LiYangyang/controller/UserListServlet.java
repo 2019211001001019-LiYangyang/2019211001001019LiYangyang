d47ebeb7149ec84383d827ee53f0b4d4d4407dc6
2019211001001019LiYangyang <1954585880@qq.com>
2021-05-30 22:10:12 +0800

    HE-week 13

---
+++ src/main/java/com/LiYangyang/controller/UserListServlet.java
@@ -0,0 +1,23 @@
+package com.LiYangyang.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet(name = "UserListServlet", value = "/admin/userList")
+public class UserListServlet extends HttpServlet {
+    Connection con=null;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path="/WEB-INF/views/admin/userList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
